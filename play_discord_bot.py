bb512da10542a1e1eccb8e83ec0d85278525feff
Dimasmedved <70750923+Dimasmedved@users.noreply.github.com>
2020-11-05 11:17:56 +0600

    Update play_discord_bot.py

--- play_discord_bot.py
+++ play_discord_bot.py
@@ -9,7 +9,7 @@ client=commands.Bot(command_prefix=PRIFIX)
 
 
 @client.command()
-async def бета(ctx,member: discord.Member,amout=1):
+async def beta(ctx,member: discord.Member,amout=1):
         await ctx.channel.purge(limit=amout)
         await ctx.author.send("Привет теперь ты попал в конкурс" + "\n" + "держи ссылку на саму игру: " + "https://yadi.sk/d/uoPHL2wck4Uc7g" + "\n" + "и удачи тебе")
         await member.add_roles(member.guild.get_role(773474507796774912))
