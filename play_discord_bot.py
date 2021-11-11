c25c054bcc6fb2022f932fcc6821544852a24bba
Dimasmedved <70750923+Dimasmedved@users.noreply.github.com>
2021-11-11 20:32:14 +0600

    Update play_discord_bot.py

--- play_discord_bot.py
+++ play_discord_bot.py
@@ -24,6 +24,8 @@ db.commit()
 keyboard_Nachalo=VkKeyboard(one_time=True)
 keyboard_Nachalo.add_button("Играть",color=VkKeyboardColor.POSITIVE)
 keyboard_Nachalo.add_line()
+keyboard_Nachalo.add_button("Игры от medved075",color=VkKeyboardColor.SECONDARY)
+keyboard_Nachalo.add_line()
 keyboard_Nachalo.add_openlink_button("YouTube",link="https://www.youtube.com/channel/UCyuR6I9fkBMMvXoCKVaMsNA")
 
 ##игра
@@ -34,8 +36,15 @@ keyboard_game.add_button("Казино",color=VkKeyboardColor.NEGATIVE)
 keyboard_game.add_button("Баланс",color=VkKeyboardColor.SECONDARY)
 keyboard_game.add_line()
 keyboard_game.add_button("Назад",color=VkKeyboardColor.PRIMARY)
-
-
+#мои игры
+keyboard_myGame=VkKeyboard(one_time=False)
+keyboard_myGame.add_button("Fly bird Fly",color=VkKeyboardColor.POSITIVE)
+keyboard_myGame.add_line()
+keyboard_myGame.add_button("ByGone DEMO VR",color=VkKeyboardColor.SECONDARY)
+keyboard_myGame.add_line()
+keyboard_myGame.add_button("Stone ball adventure",color=VkKeyboardColor.POSITIVE)
+keyboard_myGame.add_line()
+keyboard_myGame.add_button("Назад",color=VkKeyboardColor.PRIMARY)
 
 #начальная клава
 def write_key_menu(sender,message):
@@ -43,6 +52,8 @@ def write_key_menu(sender,message):
 
 def write_key_game(sender,message):
     avtorizatia.method('messages.send',{'user_id':sender,"message": message,"random_id":get_random_id(),"keyboard":keyboard_game.get_keyboard()})
+def write_key_my_game(sender,message):
+    avtorizatia.method('messages.send',{'user_id':sender,"message": message,"random_id":get_random_id(),"keyboard":keyboard_myGame.get_keyboard()})
 
 for event in longpoll.listen():
     if event.type==VkEventType.MESSAGE_NEW and event.to_me and event.text:
@@ -58,7 +69,20 @@ for event in longpoll.listen():
            else:
                write_key_game(sender, "В игре")
                ###
-
+       # мои игры
+       elif reseived_message=="Игры от medved075":
+            write_key_my_game(sender,"Добро пожаловать в игры разработчика medved075, Какую игру вы хотите выбрать?")
+       elif reseived_message=="Fly bird Fly":
+           write_key_my_game(sender,"Название: Fly bird Fly"+"\n"+
+                             "Описание: Помоги потерявшейся птице не столкнутся со стаей птиц. Это первая игра от medved075"+"\n"+
+                             "Ссылки на скачивание:"+"\n"+"AppGallery: https://appgallery.huawei.com/app/C104567695?sharePrepath=ag&locale=ru_RU&source=appshare&subsource=C104567695&shareTo=com.android.bluetooth&shareFrom=appmarket"+"\n"+
+                             "GameJolt: https://gamejolt.com/games/fly_bird_fly/634443")
+       elif reseived_message=="ByGone DEMO VR":
+           write_key_my_game(sender,"Название: ByGone DEMO VR"+"\n"+"Описание: Игра в виртуальной реальности(VR) в жанре horror. Для игры понадобится всего лишь ваш телефон и cardboard. Приятной игры !"+"\n"+
+                             "Ссылки на скачивание:"+"\n"+"AppGallry: https://appgallery.huawei.com/app/C104762829?sharePrepath=ag&locale=ru_RU&source=appshare&subsource=C104762829&shareTo=com.android.bluetooth&shareFrom=appmarket"+"\n"+
+                             "GameJolt: https://gamejolt.com/games/ByGone/647391")
+       elif reseived_message == "Stone ball adventure":
+           write_key_my_game(sender,"Название: Stone ball adventure"+"\n"+"Описание: 3D платформер про каменного шара, в игре насчитывается более 50 уровней. В настоящее время игра находится в разработке.")
         ####GAME
        elif reseived_message=="Клик":
            cur.execute(f"SELECT cash FROM users WHERE userID='{sender}'")
